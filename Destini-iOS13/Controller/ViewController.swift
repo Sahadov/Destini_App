7c24bc39571133335afb46fafe09ec5345acdd93
Dmitry <mrsahadov@gmail.com>
2024-06-06 20:33:46 -0300

    add necessary logic (not MVC)

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,8 +14,38 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    let stories = [Story(
+        title: "You see a fork in the road.",
+        choice1: "Take a left",
+        choice2: "Take a right"),
+        Story(title: "You see a tiger.",
+        choice1: "Shout for help",
+        choice2: "Play dead"),
+        Story(title: "You find a treasure chest",
+        choice1: "Open it",
+        choice2: "Check for traps")]
+    
+    var currentStoryNumber = 0
+    
+    func updateUI(){
+        storyLabel.text = stories[currentStoryNumber].title
+        choice1Button.setTitle(stories[currentStoryNumber].choice1, for: .normal)
+        choice2Button.setTitle(stories[currentStoryNumber].choice2, for: .normal)
+    }
+    
+    
+    @IBAction func choicePressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle
+        let firstChoice = stories[0].choice1
+        
+        if userAnswer == firstChoice { currentStoryNumber = 1
+        } else { currentStoryNumber = 2 }
+        updateUI()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
 
     }
 
